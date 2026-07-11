5cf91c763cf7cab45da912134acc491c408c4518
Phan Thái <phanthai18072001@gmail.com>
2026-07-11 20:41:15 +0700

    Fix portrait menu scrolling on phones

--- deploy/sw.js
+++ deploy/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tienlen-v7-install-landscape';
+const CACHE = 'tienlen-v8-portrait-scroll';
 const ASSETS = [
   './', './index.html', './styles.css', './manifest.webmanifest', './icon-192.png', './icon-512.png',
   './js/config.js', './js/engine.js', './js/state.js', './js/game.js', './js/ui.js', './js/social.js',
